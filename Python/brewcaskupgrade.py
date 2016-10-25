257adf1aafd3210f608ea8f8442fbb2051a86d88
Song Li <songl@outlook.com>
2016-10-25 18:30:41 +0800

    Do nothing without cask

--- Python/brewcaskupgrade.py
+++ Python/brewcaskupgrade.py
@@ -27,47 +27,50 @@ list_command = [
 
 list_installed = str.split(check_output(list_command).decode(), '\n')
 list_installed = [i for i in list_installed if i is not '']
-updated_count = 0
-pbar = tqdm(list_installed)
-for cask in pbar:
-    pbar.set_description(cask)
-    info_command = [
-        brew_bin,
-        'cask',
-        'info',
-        cask
-    ]
-    try:
-        install_status = str.splitlines(check_output(info_command).decode())
-    except:
-        install_status = 'Not installed'
-
-    version = str.strip(str.split(install_status[0], ':')[1])
-    is_version_installed = False
-    for line in install_status:
-        if not line.startswith(cask) and cask in line and version in line:
-            is_version_installed = True
-
-    if not is_version_installed:
-        install_command = [
+if list_installed:
+    updated_count = 0
+    pbar = tqdm(list_installed)
+    for cask in pbar:
+        pbar.set_description(cask)
+        info_command = [
             brew_bin,
             'cask',
-            'install',
-            '--force',
+            'info',
             cask
         ]
-        if args.pretend:
-            print(' '.join(install_command))
-        else:
-            if args.forceuninstall:
-                uninstall_command = [
-                    brew_bin,
-                    'cask',
-                    'uninstall',
-                    '--force',
-                    cask
-                ]
-                run(uninstall_command)
-            run(install_command)
-        updated_count += 1
-print(str(updated_count) + ' cask(s) updated')
+        try:
+            install_status = str.splitlines(check_output(info_command).decode())
+        except:
+            install_status = 'Not installed'
+
+        version = str.strip(str.split(install_status[0], ':')[1])
+        is_version_installed = False
+        for line in install_status:
+            if not line.startswith(cask) and cask in line and version in line:
+                is_version_installed = True
+
+        if not is_version_installed:
+            install_command = [
+                brew_bin,
+                'cask',
+                'install',
+                '--force',
+                cask
+            ]
+            if args.pretend:
+                print(' '.join(install_command))
+            else:
+                if args.forceuninstall:
+                    uninstall_command = [
+                        brew_bin,
+                        'cask',
+                        'uninstall',
+                        '--force',
+                        cask
+                    ]
+                    run(uninstall_command)
+                run(install_command)
+            updated_count += 1
+    print(str(updated_count) + ' cask(s) updated')
+else:
+    print('0 cask installed')
