e019f6c91a40023f34050cdc9299889cbd5fccfc
Song <songl@outlook.com>
2016-11-11 23:39:24 +0800

    Fix exception when cask is not available

--- Python/brewcaskupgrade.py
+++ Python/brewcaskupgrade.py
@@ -40,37 +40,37 @@ if list_installed:
         ]
         try:
             install_status = str.splitlines(check_output(info_command).decode())
+            version = str.strip(str.split(install_status[0], ':')[1])
+            is_version_installed = False
+            for line in install_status:
+                if not line.startswith(cask) and cask in line and version in line:
+                    is_version_installed = True
+
+            if not is_version_installed:
+                install_command = [
+                    brew_bin,
+                    'cask',
+                    'install',
+                    '--force',
+                    cask
+                ]
+                if args.pretend:
+                    print(' '.join(install_command))
+                else:
+                    if args.forceuninstall:
+                        uninstall_command = [
+                            brew_bin,
+                            'cask',
+                            'uninstall',
+                            '--force',
+                            cask
+                        ]
+                        run(uninstall_command)
+                    run(install_command)
+                updated_count += 1
         except:
-            install_status = 'Not installed'
+            print(cask + ' installation is broken')
 
-        version = str.strip(str.split(install_status[0], ':')[1])
-        is_version_installed = False
-        for line in install_status:
-            if not line.startswith(cask) and cask in line and version in line:
-                is_version_installed = True
-
-        if not is_version_installed:
-            install_command = [
-                brew_bin,
-                'cask',
-                'install',
-                '--force',
-                cask
-            ]
-            if args.pretend:
-                print(' '.join(install_command))
-            else:
-                if args.forceuninstall:
-                    uninstall_command = [
-                        brew_bin,
-                        'cask',
-                        'uninstall',
-                        '--force',
-                        cask
-                    ]
-                    run(uninstall_command)
-                run(install_command)
-            updated_count += 1
     print(str(updated_count) + ' cask(s) updated')
 else:
     print('0 cask installed')
