7b0cb2beca1a29e97139cf393f36d8533fa1b585
Song Li <songl@outlook.com>
2018-10-19 19:07:09 +0800

    Update Audacity to 2.3.0

--- homebrew/audacity.rb
+++ homebrew/audacity.rb
@@ -1,22 +1,26 @@
 cask 'audacity' do
-  version '2.2.2'
-  sha256 '39a2783c78cca13e893321e0d87098739258cabbfc257119ee6cb21d0dc9037c'
-
+  version '2.3.0'
+  sha256 '16684a03a6e6aacac0962636735ce7d9bf11483f6d194b05729981136cced9eb'
   url do
     # Audacity does not provide a fixed URL
-    # Their download URL points to a html page that generates a temporary URL embedded within an iframe
-    # 'open-uri' is required to open that page and grab the temporary URL
-    require 'open-uri'
+    # Their download URL points to a html page that post a request through js
     # fosshub.com/Audacity.html was verified as official when first introduced to the cask
-    open("https://www.fosshub.com/Audacity.html/audacity-macos-#{version}.dmg") do |io|
-      content = io.read
-      %r{^\<iframe.*src=\"(http.*\.dmg)\".*>}.match(content)[1].to_s
-    end
+    require 'net/https'
+    params = {}
+    params["projectId"] = '5b7eee97e8058c20a7bbfcf4'
+    params["releaseId"] = '5bb1fc583019e925782edd0d'
+    params["projectUri"] = 'Audacity.html'
+    params["fileName"] = "audacity-macos-#{version}.dmg"
+    params["isLatestVersion"] = 'true'
+    uri = URI.parse("https://api.fosshub.com/download/")
+    res = Net::HTTP.post_form(uri, params)
+    require 'json'
+    JSON.parse(res.body)['data']['url'].to_s
   end
   name 'Audacity'
   homepage 'http://www.audacityteam.org/'
 
-  depends_on macos: '>= :snow_leopard'
+  depends_on macos: '>= :lion'
 
   app 'Audacity.app'
 
