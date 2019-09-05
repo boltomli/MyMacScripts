63634f1795d2e0a65438075717641f9bff431005
Song Li <songl@outlook.com>
2019-09-05 10:58:47 +0800

    update foobar2000 to 2.1.50

--- homebrew/foobar2000.rb
+++ homebrew/foobar2000.rb
@@ -1,7 +1,7 @@
 cask 'foobar2000' do
-  version '2.1.27'
-  sha256 'b96cc690cd17dc60bb325bbf66110f4c990081d6ef0d1e17387bca3f4047f321'
-  url "https://www.foobar2000.org/files/10f355c80cfe611d40aa37f0392580fb/foobar2000-v#{version}.dmg"
+  version '2.1.50'
+  sha256 '47b679524ea80aed30ce3505ec3983902c8bb0ce1eb1574c5f9c80bd9dcf4127'
+  url "https://www.foobar2000.org/files/17d4961b0f70f340995962a78d428410/foobar2000-v#{version}.dmg"
   name 'foobar2000'
   homepage 'https://www.foobar2000.org/mac'
   depends_on macos: '>= :mavericks'
